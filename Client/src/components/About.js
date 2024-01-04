191c736e4103e952420056c6bffec33c4cb579ee
Abhishek wani <75797884+Abhishek001-1@users.noreply.github.com>
2024-01-05 01:54:01 +0530

    Update About.js
    updated about page

--- Client/src/components/About.js
+++ Client/src/components/About.js
@@ -1,13 +1,21 @@
+import React from 'react';
 
 const About = () => {
-const handleClick = ()=>{
-  localStorage.removeItem("token")
-}
-    return (
-      <>
-   <button onClick={handleClick}> button</button>
-      </>
-    )
+  const handleClick = () => {
+    localStorage.removeItem("token");
+  }
+
+  return (
+    <div className="about-container">
+      <h2>About iNotebook</h2>
+      <p>
+        Welcome to iNotebook - Your Secret Noteskeeping Companion! iNotebook is a secure
+        and feature-rich application designed to help you organize and manage your notes
+        with the utmost privacy.
+      </p>
+      
+    </div>
+  );
 }
 
-export default About
+export default About;
