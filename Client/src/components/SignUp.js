7097122b069d8329dd8ef9c8f00b458225463c51
Abhishek wani <75797884+Abhishek001-1@users.noreply.github.com>
2024-01-23 11:00:59 +0530

    Update SignUp.js

--- Client/src/components/SignUp.js
+++ Client/src/components/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = () => {
 
   const handleClick = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/auth/createuser', {
+    const response = await fetch('https://inotebook-server-two.vercel.app/createuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
